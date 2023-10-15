202955d867df7814ec6c95a6844c42627365da5c
Wojtek <plonkawojtek57@gmail.com>
2023-10-15 16:30:41 +0200

    lekcja 2 poczatek ale tak srednio

--- Plonka_Aplikacje_Mobline/My Application2/app/src/main/java/com/example/myapplication2/MainActivity.java
+++ Plonka_Aplikacje_Mobline/My Application2/app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.myapplication2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_MESSAGE = "com.example.myapplication.Message";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editTextText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,message);
+        startActivity(intent);
+
     }
 }
